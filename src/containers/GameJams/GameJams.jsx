665d16d7b17195bccef9eca88d56483e6f6f4ed6
raphkwok <raphael.ch.kwok@gmail.com>
2023-04-20 12:08:46 -0400

    Fixed image assets and start on gallery

--- src/containers/GameJams/GameJams.jsx
+++ src/containers/GameJams/GameJams.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 import './GameJams.css'
-import { images } from '../../constants/images.js';
+import images from '../../constants/images.js';
 
 const GameJams = () => {
+  
+  const galleryImages = [
+    images.altair, images.nightdrive, images.hijack, images.office,
+    images.diceroll, images.altair
+  ]
+  
   return (
     <div className='app__gamejam section__padding' id='jams'>
       <div className='app_gamejam-title'>
@@ -11,7 +17,11 @@ const GameJams = () => {
       </div>
       <div className='section__divider'></div>
       <div className='app_gamejam-gallery'>
-
+        {galleryImages.map((image, index) => (
+          <div className='app_gamejam-image' key={index}>
+            <img src={image} alt='gamejam' />
+          </div>
+        ))}
       </div>
     </div>
   )
